4f3ff95c117528bcee5788f3c577657de0888753
mdeshina <deshinamash>
2020-09-13 11:32:23 +0300

    refactoring

--- WeatherApp/Application/SceneDelegate.swift
+++ WeatherApp/Application/SceneDelegate.swift
@@ -4,7 +4,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
@@ -17,30 +16,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let configurator = CitiesConfigurator()
         configurator.configure(viewController: listOfCitiesTableVC)
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
 }
 
